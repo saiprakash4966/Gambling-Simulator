41949f32bafe29d99e9b081ed25f4649bc6392d8
unknown <saiprakashbeemari@gmail.com>
2022-02-19 08:36:10 +0530

    Gaming simulator

---
+++ Gambler.java
@@ -0,0 +1,19 @@
+package com.bl;
+
+import java.util.Scanner;
+
+public class Gambler
+{
+	public static void main(String[]args)
+	{
+		int a,b;
+		Scanner sc = new Scanner(System.in);
+		{
+			System.out.println("a For win stake  ");
+		}
+	
+		
+	System.out.println("b for loose stake ");
+	}
+
+}
